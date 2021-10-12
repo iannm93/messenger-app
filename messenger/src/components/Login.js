eb7b92bbf4e3993c1bb2392e85d52299c186ed41
Ian Harris <ianmharris93@gmail.com>
2021-10-11 19:51:20 -0700

    set up Login/ new ID page

--- messenger/src/components/Login.js
+++ messenger/src/components/Login.js
@@ -1,18 +1,21 @@
+
 import React, {useRef} from 'react'
-import { Container, Form} from 'react-bootstrap'
+import { Container, Form, Button} from 'react-bootstrap'
 
 
 export default function Login (){
     const idRef = useRef()
     return(
-        <Container>
-            <Form>
+        <Container className ="align-items-center d-flex" style = {{height:"100vh"}} >
+            <Form className= "w-100">
                 <Form.Group>
                     <Form.Label>
                         ID Here
                     </Form.Label>
-                    <Form.Control ref= {idRef}type = "text"></Form.Control>
+                    <Form.Control ref= {idRef} required type = "text"></Form.Control>
                 </Form.Group>
+                <Button type="submit" className="mr-2">Login</Button>
+                <Button variant= "secondary">New ID</Button>
 
             </Form>
         </Container>
